215d145a1e6f59ab49eecc1a706511f69a5d455f
S3bTheGuy <sebastian1511@icloud.com>
2024-06-01 00:46:50 +0200

    added proper 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,23 +2,16 @@ import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Not Found</h1>
+    <main class="text-center mx-auto text-primary p-4 bg-base-100">
+      <h1 class="max-6-xs text-6xl text-primary font-thin uppercase my-16">Not Found</h1>
       <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
+        Oops! The page you're looking for doesn't exist.
       </p>
       <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
+        You can return to the{" "}
+        <A href="/" class="text-accent font-bold hover:underline">
+          home page
+        </A>{" "}
       </p>
     </main>
   );
